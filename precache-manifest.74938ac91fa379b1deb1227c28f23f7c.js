12c67cd9566981ae0a46a2e1a59863320d8e0f6e
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-06-24 09:31:05 +0300

    Updates

---
+++ precache-manifest.74938ac91fa379b1deb1227c28f23f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb987c5aad4e5a7f31198b5a72f2c1a",
+    "url": "/tours-gallery/index.html"
+  },
+  {
+    "revision": "53ce43d1403dbd314d38",
+    "url": "/tours-gallery/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tours-gallery/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tours-gallery/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ce43d1403dbd314d38",
+    "url": "/tours-gallery/static/js/main.4dfc61be.chunk.js"
+  },
+  {
+    "revision": "30e49ee9bdd5d52fadd6",
+    "url": "/tours-gallery/static/js/runtime-main.7ca29b51.js"
+  }
+]);
